775f7c3ffd14fae6220724761e008a991565ba7d
Jesus Milan <jmilan.gonzalez@gmail.com>
2021-03-07 09:28:00 -0500

    created employer database

---
+++ schema.sql
@@ -0,0 +1,9 @@
+-- deletes database if the same name is already used
+DROP DATABASE IF EXISTS employer_db;
+
+-- Creates new database
+CREATE DATABASE employer_db;
+
+-- selects the database to be worked with
+USE employer_db;
+
