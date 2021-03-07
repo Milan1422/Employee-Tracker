2201367e14b92697265bb1d59d143828a53a3410
Jesus Milan <jmilan.gonzalez@gmail.com>
2021-03-07 09:34:45 -0500

    created employee table in employer db

--- schema.sql
+++ schema.sql
@@ -7,3 +7,12 @@ CREATE DATABASE employer_db;
 -- selects the database to be worked with
 USE employer_db;
 
+-- creates employee table in employer database
+CREATE TABLE employees (
+    id INT AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT,
+    PRIMARY KEY(id)
+)
