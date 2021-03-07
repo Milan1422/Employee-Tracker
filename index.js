c01ee9fcdfe8f5bdbf799f6be98f46f4668bb2c3
Jesus Milan <jmilan.gonzalez@gmail.com>
2021-03-07 11:44:34 -0500

    created a succesfull connection to mysql and selected employer database

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
+// dependencies that will be used with the application
 const mysql = require('mysql');
 const inquirer = require('inquirer');
 const express = require('express');
 const consoleTable = require('console.table');
+
+// extablishing a connection to mysql
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'rootgtboot',
+    database: 'employer_db'
+});
+
+connection.connect((err) => {
+    if (err) throw err;
+    console.log('connection succesfull!');
+})
