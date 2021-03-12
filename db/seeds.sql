2bf643aa97ad5b7e148ded843de1ba5c05a8e424
Jesus Milan <jmilan.gonzalez@gmail.com>
2021-03-11 23:49:10 -0500

    added values to department table

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,14 +1,16 @@
 -- we utilize the USE keyword to select our database
 USE employer_db;
 
--- we use this block of code to insert new rows to the employee table
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ('John', 'Doe', 1, 1);
-
--- we use this block of code to insert new rows to the role table
-INSERT INTO role (title, salary, department_id)
-VALUES ('Manager', 90000, 1);
-
--- we use this block of code to insert new rows to the role table
+-- we use this block of code to insert new rows to the department table
 INSERT INTO department (name)
-VALUE ('Sales');
+VALUES ('Sales');
+
+INSERT INTO department (name)
+VALUES ('Engineering');
+
+INSERT INTO department (name)
+VALUES ('Finance');
+
+INSERT INTO department (name)
+VALUES ('Legal');
+
