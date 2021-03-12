9f61e64837543386059c60c66d918e1ab5c88a4f
Jesus Milan <jmilan.gonzalez@gmail.com>
2021-03-11 22:17:02 -0500

    added role seed

--- db/seeds.sql
+++ db/seeds.sql
@@ -4,3 +4,7 @@ USE employer_db;
 -- we use this block of code to insert new rows to the employee table
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES ('John', 'Doe', 1, 1);
+
+-- we use this block of code to insert new rows to the role table
+INSERT INTO role (title, salary, department_id)
+VALUES ('Manager', 90000, 1);
