e3ab92dbc50218983d3fd16385d4a7d60b790a99
Jesus Milan <jmilan.gonzalez@gmail.com>
2021-03-11 23:43:11 -0500

    adjusted schema.sql to accept foreign keys

--- db/schema.sql
+++ db/schema.sql
@@ -7,13 +7,10 @@ CREATE DATABASE employer_db;
 -- selects the database to be worked with
 USE employer_db;
 
--- creates employee table in employer database
-CREATE TABLE employee (
+-- creates department table
+CREATE TABLE department (
     id INT AUTO_INCREMENT,
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
-    role_id INT NOT NULL,
-    manager_id INT,
+    name VARCHAR(30) NOT NULL,
     PRIMARY KEY(id)
 );
 
@@ -22,13 +19,21 @@ CREATE TABLE role (
     id INT AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL (8,2) NOT NULL,
-    department_id INT NOT NULL,
-    PRIMARY KEY(id)
+    department INT,
+    PRIMARY KEY(id),
+    FOREIGN KEY (department) REFERENCES department(id)
 );
 
--- creates department table
-CREATE TABLE department (
+-- creates employee table in employer database
+CREATE TABLE employee (
     id INT AUTO_INCREMENT,
-    name VARCHAR(30) NOT NULL,
-    PRIMARY KEY(id)
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    title INT,
+    manager INT,
+    PRIMARY KEY(id),
+    FOREIGN KEY (title) REFERENCES role(id),
+	FOREIGN KEY (manager) REFERENCES employee(id)
 );
+
+
